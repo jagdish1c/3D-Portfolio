4f96ccbfbb836b1f45370266584707820ab3d1d8
Jagdish <chandrajagdish812@gmail.com>
2025-06-28 11:49:05 +0530

    Joystick added

--- src/components/Joystick.js
+++ src/components/Joystick.js
@@ -2,5 +2,30 @@ import React from 'react';
 import ReactNipple from 'react-nipple';
 
 const Joystick = ({ onMove, onEnd }) => {
-  
-}
+  return (
+    <div style={{
+      position: 'absolute',
+      bottom: '20px',
+      left: '20px',
+      width: '120px',
+      height: '120px'
+    }}>
+      <ReactNipple
+        options={{
+          mode: 'static',
+          position: { top: '50%', left: '50%' },
+          color: 'blue',
+          size: 120
+        }}
+        style={{
+          width: '120px',
+          height: '120px'
+        }}
+        onMove={(evt, data) => onMove && onMove(data)}
+        onEnd={() => onEnd && onEnd()}
+      />
+    </div>
+  );
+};
+
+export default Joystick;
